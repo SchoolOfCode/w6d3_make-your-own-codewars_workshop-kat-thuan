115c21cc921c540eb67d90b052e48454822b6dcd
thuannguyen88 <getaholdofthuan@gmail.com>
2021-12-22 15:38:59 +0000

    decided on code wars challenge and wrote out instructions for participants

--- main.js
+++ main.js
@@ -3,3 +3,48 @@
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+
+
+
+
+/*
+School of Code Grade
+
+takes inputs of (projects, daily test)
+project score - 90+
+daily test completed- 40-48
+return final grade - "A - You're getting hired"
+
+project score - 80-89
+daily test completed- 30-39
+return final grade - "B - You're doing well"
+
+project score - 60-79
+daily test completed- 20-29
+return final grade - "C - Come to code camp"
+
+project score - Less than 60
+daily test completed- Less than 20
+return final grade - "F - Do your recap tasks again""
+
+Create a function called SOCfinalGrade, which calculates the final grade of a student depending on two parameters: a grade for the project and a number of completed daily tests.
+
+This function should take two arguments: project- grade for project (from 0 to 100); daily tests - number of completed tests (from 0 to 48);
+
+This function should return a string (final grade). There are four types of final grades:
+
+- If project score is over 90 and the number of daily tests completed is over 40, then give them the final grade "A - You're getting hired "
+
+- If project score is between 80-89 and the number of daily tests completed is 30-39, then give them the final grade "B - You're doing well "
+
+- If project score is between 60-79 and the number of daily tests completed is 20-29, then give them the final grade of "C - Come to code camp"
+
+- If project score is less than 60 and the number of daily tests completed is less than 20, then give them the final grade "F - Do your recap tasks again"
+
+Examples(Inputs-->Output):
+
+100, 42 --> "A - You're getting hired "
+65, 25 --> "C - Come to code camp"
+
+*/
